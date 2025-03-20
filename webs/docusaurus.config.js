d672bf384248739bbcbc04e028599b595a76c76b
juliobonjour <julienbourgeois56@free.fr>
2025-03-20 22:30:02 +0100

    sailing

--- webs/docusaurus.config.js
+++ webs/docusaurus.config.js
@@ -11,7 +11,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'My Site Julien',
-  tagline: 'Documentation is cool',
+  tagline: 'Sailing is cool',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
