d68dcbcf06824df3517bf44ccfa14e7031ab7772
juliobonjour <julienbourgeois56@free.fr>
2025-03-20 22:33:51 +0100

    Deploy website - based on d672bf384248739bbcbc04e028599b595a76c76b

---
+++ build/assets/js/acecf23e.f252fd25.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwebs=self.webpackChunkwebs||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/site/blog","blogTitle":"Blog","authorsListPath":"/site/blog/authors"}')}}]);
