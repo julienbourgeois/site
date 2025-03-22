3b2acfdca85fc885ef87c39b0edafef81f393bd1
juliobonjour <julienbourgeois56@free.fr>
2025-03-22 18:37:16 +0100

    title

--- webs/docusaurus.config.js
+++ webs/docusaurus.config.js
@@ -10,7 +10,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site Julien',
+  title: 'Bag Melen Julien',
   tagline: 'Sailing is cool',
   favicon: 'img/favicon.ico',
 
@@ -76,7 +76,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'BAG',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
